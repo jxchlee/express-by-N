4e3bbef453cbd686eb09f1fba9fbb5da288a47ed
eunsagn <dmstkdrmatkd@naver.com>
2020-09-09 10:04:55 +0000

    add.... something... i don't know it'll be work.

--- main.cpp
+++ main.cpp
@@ -26,9 +26,9 @@ int solution(int N, int number) {
 
   jump:
   
-  for(int j=min; j<8;j++){
-    if(calculate(j, number)){
-      return answer = j;
+  for(int T=min; T<8;T++){
+    if(calculate(T, number)){
+      return answer = T;
     }
   }
 
@@ -75,7 +75,7 @@ int main(){
 
 
 
-void insert(int N){
+void insert(int N){ //1,11,111 등의 값을 각 배열 첫번째에 넣음
   int num;
   for(int i=0; i<8; i++){
     num = 0;
@@ -87,15 +87,99 @@ void insert(int N){
 }
 
 
-bool calculate(int j, int number){
+bool calculate(int T, int number){
 
+  
+  if(number == plus(n[K][i], n[T-K][j])){
+    return 1;
+  }
 
+  else{
+
+  }
+  if(number == minus(n[K][i], n[T-K][j])){
+    return 1;
+  }
+  else{
+
+  }
+  if(number == multiple(n[K][i], n[T-K][j])){
+    return 1;
+  }
+  else{
+
+  }
+  if(number == divide(n[K][i], n[T-K][j])){
+    return 1;
+  }
+  else{
+    
+  }
   
   
 }
 
 
 
+operation(int T){
+  
+  for(int k=0; k<T; k++){
+    int K = T-k;
+    for(int i=0; i < n[K].capacity; i++){
+      for(int j=0; j<n[T-K].capacity;j++){
+        int tem = plus(n[K][i], n[T-K][j]);
+        if(number == tem){
+          return 1;
+        }
+        isThisNew(tem);
 
+        tem = minus(n[K][i], n[T-K][j]);
+        if(number == tem){
+          return 1;
+        }
+        isThisNew(tem);
+
+        tem = multiple(n[K][i], n[T-K][j]);
+        if(number == tem){
+          return 1;
+        }
+        isThisNew(tem);
+
+        tem = divide(n[K][i], n[T-K][j]);
+        if(number == tem){
+          return 1;
+        }
+        isThisNew(tem);
+
+
+      }
+    }
+  }
+  
+}
+
+
+
+
+int plus(int m, int n){
+  
+  
+}
+
+
+void isThisNew(int tem){
+
+
+
+    for(int i=0; i<T; i++){
+      for(int j=0; j<n[i].capacity();j++){
+        if(tem == n[i][j]){
+          goto notnew;
+        }
+      }
+    }
+    n[T].push_back(tem);
+  }
+}
 
 
